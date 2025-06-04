4f74fbbcfb0309b30406ee0dcb4021d32dfcbd51
DerouillonAlice <contact@alice.sh>
2025-06-04 13:19:38 +0200

    add-feat: implement sidebar toggle functionality and refactor Sidebar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,28 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+import fond from './assets/fond.png'
+import { useSidebar } from './composables/useSidebar'
+
+const { isSidebarOpen } = useSidebar()
+</script>
+
 <template>
   <div
     id="app"
-    class="min-h-screen flex flex-col bg-cover bg-center"
+    class="h-screen flex bg-cover bg-center"
     :style="{ backgroundImage: `url('${fond}')` }"
   >
-    <div class="flex flex-1">
-      <Sidebar class="w-64 h-screen flex-shrink-0" />
-      <main class="flex-1">
-        <Navbar />
+    <Sidebar
+      class="transition-all duration-300 h-screen flex-shrink-0"
+      :class="isSidebarOpen ? 'w-64' : 'w-12'"
+    />
+
+    <div class="flex flex-col flex-1 min-w-0 h-screen">
+      <Navbar />
+      <main class="flex-1 overflow-y-auto p-6">
         <router-view />
       </main>
     </div>
   </div>
 </template>
-
-<script setup>
-import Navbar from './components/Navbar.vue'
-import Sidebar from './components/Sidebar.vue'
-import fond from './assets/fond.png'
-</script>
