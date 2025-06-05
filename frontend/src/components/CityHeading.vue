fae9d8c17d70fad84cb5dce1130e50ac87bff9ea
Léna Pignolet <lena.pignolet@dnd.fr>
2025-06-05 11:30:17 +0200

    add-feat: add current city section

---
+++ frontend/src/components/CityHeading.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="flex items-center justify-center w-full h-16 bg-white/40 backdrop-blur-md rounded-md shadow overflow-hidden backdrop-blur-md shadow-xl border border-white/70">
+        <h1 class="text-2xl font-bold">Paris</h1>
+  </section>
+</template>
+
+<script setup>
+</script>
