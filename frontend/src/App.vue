94fca83f299c517bd317f9b8a055606c41813e32
DerouillonAlice <contact@alice.sh>
2025-06-22 13:50:50 +0200

    add-feat: implement draggable reorder functionality for favorite cities

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -137,7 +137,7 @@ const openAddFavoriteModal = inject('openAddFavoriteModal', () => {})
             class="flex justify-center"
           >
             <div
-              class="w-full mx-4 bg-white/60 rounded-xl shadow flex flex-row items-center px-3 py-2 cursor-pointer"
+              class="w-full mx-4 bg-white/40 rounded-xl shadow flex flex-row items-center px-3 py-2 cursor-pointer"
               @click="goToCity(city.name)">
               <img v-if="city.imageSrc" :src="city.imageSrc" :alt="city.name" class="w-8 h-8 object-contain mr-2" />
               <div class="flex flex-col flex-1 min-w-0">
